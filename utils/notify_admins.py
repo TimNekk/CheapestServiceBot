58547b8cfee8ca2d3028182f712386654e00d4b2
Tim <herew26@gmail.com>
2022-07-15 17:02:50 +0300

    Notifying added

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -1,14 +1,13 @@
-import logging
-
 from aiogram import Dispatcher
+from aiogram_broadcaster import TextBroadcaster
 
-from data.config import ADMINS
+from data.config import DEVELOPER, ADMINS
 
 
 async def on_startup_notify(dp: Dispatcher):
-    for admin in ADMINS:
-        try:
-            await dp.bot.send_message(admin, "Бот Запущен")
+    await dp.bot.send_message(DEVELOPER, "Бот Запущен")
 
-        except Exception as err:
-            logging.exception(err)
+
+async def notify_admins(message: str):
+    await TextBroadcaster(ADMINS, message).run()
+
