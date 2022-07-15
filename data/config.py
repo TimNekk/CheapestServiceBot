fe0e51e86ccf2ee73813defb8b398536ee4fb22a
Tim <herew26@gmail.com>
2022-07-15 17:00:23 +0300

    .env reading added

--- data/config.py
+++ data/config.py
@@ -1,10 +1,51 @@
+from dataclasses import dataclass
+
 from environs import Env
 
 # Теперь используем вместо библиотеки python-dotenv библиотеку environs
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+BOT_TOKEN = env.str("BOT_TOKEN")
+DEVELOPER = env.str("DEVELOPER")
+ADMINS = env.list("ADMINS")
 
+ADMIN_NICKNAME = env.str("ADMIN_NICKNAME")
+FEEDBACK_NICKNAME = env.str("FEEDBACK_NICKNAME")
+
+VAK_SMS_API_KEY = env.str("VAK_SMS_API_KEY")
+
+
+@dataclass
+class Qiwi:
+    secret_key: str = env.str("QIWI_SECRET_KEY")
+    theme_code: str = env.str("QIWI_THEME_CODE")
+
+
+@dataclass
+class CommandInfo:
+    command: str
+    description: str
+
+
+@dataclass
+class Commands:
+    buy = CommandInfo("buy", "Купить")
+    guide = CommandInfo("guide", "Инструкция по применению")
+    support = CommandInfo("support", "Поддержка")
+    feedback = CommandInfo("feedback", "Отзывы")
+
+    @dataclass
+    class Admin:
+        settings = CommandInfo("admin", "Админка")
+        send = CommandInfo("send", "Рассылка")
+        stats = CommandInfo("stats", "Статистика")
+
+
+@dataclass
+class Subscription:
+    id: str = env.str("SUBSCRIPTION_ID")
+    url: str = env.str("SUBSCRIPTION_URL")
+
+
+API_DOCS_URL = "https://vak-sms.com/api/vak/"
