4de61448a0e790f60382082f56e667dcd41f0b7c
Tim <herew26@gmail.com>
2022-07-15 17:03:01 +0300

    Commands updated

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -1,10 +1,26 @@
 from aiogram import types
+from aiogram.types import BotCommandScopeDefault, BotCommandScopeChat
+
+from data.config import Commands, ADMINS
 
 
 async def set_default_commands(dp):
-    await dp.bot.set_my_commands(
-        [
-            types.BotCommand("start", "Запустить бота"),
-            types.BotCommand("help", "Вывести справку"),
-        ]
-    )
+    default_commands = [
+        types.BotCommand(Commands.buy.command, Commands.buy.description),
+        types.BotCommand(Commands.guide.command, Commands.guide.description),
+        types.BotCommand(Commands.support.command, Commands.support.description),
+        types.BotCommand(Commands.feedback.command, Commands.feedback.description)
+    ]
+
+    await dp.bot.set_my_commands(commands=default_commands,
+                                 scope=BotCommandScopeDefault())
+
+    admin_commands = [
+        types.BotCommand(Commands.Admin.settings.command, Commands.Admin.settings.description),
+        types.BotCommand(Commands.Admin.send.command, Commands.Admin.send.description),
+        types.BotCommand(Commands.Admin.stats.command, Commands.Admin.stats.description),
+    ]
+
+    for admin_id in ADMINS:
+        await dp.bot.set_my_commands(commands=admin_commands + default_commands,
+                                     scope=BotCommandScopeChat(admin_id))
