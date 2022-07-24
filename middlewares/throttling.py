a7d605886b9cc38d506f9b97f02ad0a4d86e70bf
Tim <herew26@gmail.com>
2022-07-24 16:08:08 +0300

    Anti-flood added

--- middlewares/throttling.py
+++ middlewares/throttling.py
@@ -22,7 +22,7 @@ class ThrottlingMiddleware(BaseMiddleware):
     async def on_pre_process_update(self, update: types.Update, data: dict):
         if update.message.chat.id in ANTI_FLOOD_USERS:
             print("STOPPED")
-            CancelHandler()
+            raise CancelHandler()
         else:
             print("NOT STOPPED")
 
