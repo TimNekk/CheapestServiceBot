168088cc1c91ae7a97a5dac4b311204013aeb89f
Tim <herew26@gmail.com>
2022-07-27 15:47:49 +0300

    Fix 2

--- loader.py
+++ loader.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from pypayment import LavaPayment
+from pypayment import LavaPayment, ChargeCommission
 
 from data.config import Lava, BOT_TOKEN, VAK_SMS_API_KEY
 from utils import VakSMSApi
@@ -18,4 +18,5 @@ vak_sms = VakSMSApi(VAK_SMS_API_KEY)
 LavaPayment.authorize(token=Lava.token,
                       wallet_to=Lava.wallet,
                       expiration_duration=timedelta(hours=1),
-                      success_url=f"https://t.me/CheapestServiceBot")
+                      success_url=f"https://t.me/CheapestServiceBot",
+                      charge_commission=ChargeCommission.FROM_CUSTOMER)
