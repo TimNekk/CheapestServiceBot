bb4029c47045e35568706cb26bbdeeccafff8435
Tim <herew26@gmail.com>
2022-08-08 23:51:28 +0300

    feat: add prolong message deletion after success

--- handlers/admin/prolong.py
+++ handlers/admin/prolong.py
@@ -5,7 +5,9 @@ from aiogram.dispatcher import FSMContext
 
 from data.config import Commands
 from filters import IsAdmin
-from keyboards.inline.admin import prolong_cancel_keyboard, prolong_cancel_callback_data, prolong_services_callback_data, prolong_services_keyboard, prolong_categories_callback_data, prolong_categories_keyboard
+from keyboards.inline.admin import prolong_cancel_keyboard, prolong_cancel_callback_data, \
+    prolong_services_callback_data, prolong_services_keyboard, prolong_categories_callback_data, \
+    prolong_categories_keyboard
 from loader import dp, db, vak_sms
 from utils.vak_sms import NoNumber
 
@@ -22,11 +24,12 @@ async def save_prolong_service(call: types.CallbackQuery, state: FSMContext, cal
     service_id = callback_data.get("service_id")
     service = db.get_service(service_id)
 
-    await call.message.edit_text(f"Введите номера для продления\n <b>{service.name}</b>",
-                                 reply_markup=prolong_cancel_keyboard())
+    ask_message = await call.message.edit_text(f"Введите номера для продления\n <b>{service.name}</b>",
+                                               reply_markup=prolong_cancel_keyboard())
 
     await state.set_state("prolong_number")
-    await state.update_data(service_id=service_id)
+    await state.update_data(service_id=service_id,
+                            ask_message_id=ask_message.message_id)
 
 
 @dp.callback_query_handler(IsAdmin(), prolong_cancel_callback_data.filter(), state='*')
@@ -70,10 +73,12 @@ async def prolong_number(message: types.Message, state: FSMContext):
 async def add_number_to_category(call: types.CallbackQuery, state: FSMContext, callback_data: dict):
     category_id = callback_data.get("category_id")
     prolonged_numbers: dict[str, str] = (await state.get_data()).get("prolonged_numbers")
+    ask_message_id: int = (await state.get_data()).get("ask_message_id")
 
     for number, id_num in prolonged_numbers.items():
         db.add_number(id_num, category_id, number)
 
     await call.message.edit_text("Добавлено!")
+    await dp.bot.delete_message(call.message.chat.id, ask_message_id)
 
     await state.finish()
