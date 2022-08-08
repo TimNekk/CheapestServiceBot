cc64bad93257958c5858cf926652756779a566a2
Tim <herew26@gmail.com>
2022-08-08 21:40:04 +0300

    feat: add callback support for Admin filter

--- filters/admin.py
+++ filters/admin.py
@@ -5,5 +5,11 @@ from data.config import ADMINS
 
 
 class IsAdmin(BoundFilter):
-    async def check(self, message: types.Message):
+    async def check(self, event):
+        if isinstance(event, types.Message):
+            message = event
+        elif isinstance(event, types.CallbackQuery):
+            message = event.message
+        else:
+            raise NotImplementedError("Unknown event")
         return str(message.chat.id) in ADMINS
