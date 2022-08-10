bf052388fdeb1559fe15aca5d002db95ff2c93de
Tim <herew26@gmail.com>
2022-08-10 21:29:26 +0300

    feat: add file logger

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import sys
+from datetime import time
+
 from aiogram import executor
+from loguru import logger
 
 from loader import dp
 import middlewares, filters, handlers
@@ -7,6 +11,10 @@ from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
+    logger.remove()
+    logger.add(sys.stderr, level="INFO")
+    logger.add(f"logs/CSB.log", rotation="00:00", retention="3 days", level="DEBUG")
+
     # Устанавливаем дефолтные команды
     await set_default_commands(dispatcher)
 
